b8bebe44609d71e6cf201aa6db05c5853d0f304d
发财树 <facaishu@facaishudeMac-mini.local>
2026-05-02 22:40:59 +1000

    your message

--- F to C.py
+++ F to C.py
@@ -1,3 +1,12 @@
-tempreture = eval(input("Enter the temperature in Fahrenheit: "))
-celsius = (tempreture - 32) * 5.0/9.
-print(f"The temperature in Celsius is about: {celsius:.2f}°C")
+while True:
+    f_or_c = input("Enter F or C: ")
+    if f_or_c == "F":
+        f = float(input("Enter the temperature in F: "))
+        c = (f - 32) * 5 / 9
+        print(f"{f} Fahrenheit is {c:.2f} degrees Celsius to 2 decimal points.")
+    elif f_or_c == "C":
+        c = float(input("Enter the temperature in C: "))
+        f = (c * 9 / 5) + 32
+        print(f"{c} degrees Celsius is {f:.2f} Fahrenheit to 2 decimal points.")
+    else:
+        print("Invalid input. Please enter F or C.")
--- Tip Calculator.py
+++ Tip Calculator.py
@@ -1,5 +1,5 @@
-meal_cost = float(input("Enter the meal cost: "))
-tip_percentage = float(input("Enter the tip percentage: "))
+meal_cost = eval(input("Enter the meal cost: "))
+tip_percentage = eval(input("Enter the tip percentage: "))
 tip_amount = meal_cost * (tip_percentage / 100)
 total_cost = meal_cost + tip_amount
 print(f"Tip amount: ${tip_amount:.2f}")
